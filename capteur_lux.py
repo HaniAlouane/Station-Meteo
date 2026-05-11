3087d0c0f08952de58875c11bb7443758ab56a57
HaniAlouane <alouanehani23@gmail.com>
2026-05-11 15:20:26 +0200

    capteur_lux.py

--- capteur_lux.py
+++ capteur_lux.py
@@ -1,9 +1,6 @@
-# =================================================================
-# 💡 PILOTE DU CAPTEUR DE LUMIÈRE NUMÉRIQUE (VEML7700)
-# =================================================================
 from machine import Pin, I2C
 import time
-import veml7700 # Importation de la bibliothèque spécifique au capteur
+import veml7700
 
 class CapteurLumiere:
     """
@@ -12,21 +9,13 @@ class CapteurLumiere:
     """
     
     def __init__(self, i2c_objet, periode_ms=2000):
-        # 1. RÉCUPÉRATION DU BUS I2C :
-        # On utilise l'objet I2C déjà initialisé dans le fichier principal.
         self.i2c = i2c_objet
         
-        # 2. INITIALISATION DU CAPTEUR :
-        # La bibliothèque veml7700 établit la communication avec l'adresse 
-        # matérielle du capteur (généralement 0x10).
         self.capteur = veml7700.VEML7700(i2c=self.i2c)
 
-        # 3. GESTION DU TIMING :
-        # On définit la fréquence de lecture (toutes les 2 secondes par défaut).
         self.periode_ms = periode_ms
         self.dernier_temps = time.ticks_ms()
         
-        # 4. STOCKAGE : Variable pour conserver la dernière valeur de Lux lue.
         self.lux = 0.0
 
     def lire(self):
@@ -34,8 +23,7 @@ class CapteurLumiere:
         Interroge le capteur pour obtenir la luminosité actuelle.
         Retourne la valeur en Lux (unité internationale).
         """
-        # On utilise la méthode de la bibliothèque pour lire la valeur convertie.
-        # Le capteur gère lui-même les gains et le temps d'intégration en interne.
+        
         self.lux = self.capteur.read_lux()
         return self.lux
 
@@ -47,11 +35,9 @@ class CapteurLumiere:
         """
         maintenant = time.ticks_ms()
         
-        # Si le temps écoulé dépasse la période définie (2000ms)
         if time.ticks_diff(maintenant, self.dernier_temps) >= self.periode_ms:
-            self.dernier_temps = maintenant # Mise à jour du chronomètre interne
+            self.dernier_temps = maintenant
             
-            # Déclenchement de la lecture physique
             valeur = self.lire()
             
             # Renvoie la valeur et "True" pour signaler qu'une mise à jour a eu lieu
